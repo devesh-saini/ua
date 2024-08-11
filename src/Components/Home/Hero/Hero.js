5ed23c2c0dbaab109ba75c831c8cbafd14ab4d1b
d3v3sh 54ini <128877193+devesh-saini@users.noreply.github.com>
2024-08-11 20:12:02 +0530

    hero-section

--- src/Components/Home/Hero/Hero.js
+++ src/Components/Home/Hero/Hero.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import './Hero.css'
-// import p1 from '../../Assets/HeroPics/TestPics/BG1.png'
-// import p2 from '../../Assets/HeroPics/TestPics/BG2.png'
-// import p3 from '../../Assets/HeroPics/TestPics/BG3.png'
-// import p4 from '../../Assets/HeroPics/TestPics/BG4.png'
+import p1 from '../../Assets/HeroPics/TestPics/BG1.png'
+import p2 from '../../Assets/HeroPics/TestPics/BG2.png'
+import p3 from '../../Assets/HeroPics/TestPics/BG3.png'
+import p4 from '../../Assets/HeroPics/TestPics/BG4.png'
 
 function Hero() {
     return (
         <>
             <div className="heroMain min-h-[100vh]">
-                <div className="circle-areas pt-14 flex">
-                    <div className="circle-area w-[100px] h-[100px] flex flex-col justify-center items-center">
-                        <div className="circle bg-gray-200 size-[20vw] rounded-full"></div>
+                <div className="circle-areas pt-14 flex justify-center">
+                    <div className="circle-area w-[100px] h-[100px] md:h-[250px] md:w-[250px] flex flex-col justify-center items-center">
+                        <div className="circle bg-gray-200 size-[65px] md:size-[150px] rounded-full overflow-hidden">
+                            <img src={p1} alt="" />
+                        </div>
                     </div>
-                    <div className="circle-area w-[100px] h-[100px] flex flex-col justify-center items-center">
-                        <div className="circle bg-gray-200 size-[20vw] rounded-full"></div>
+                    <div className="circle-area w-[100px] h-[100px] md:h-[250px] md:w-[250px] flex flex-col justify-center items-center">
+                        <div className="circle bg-gray-200 size-[65px] md:size-[150px] rounded-full overflow-hidden">
+                            <img src={p2} alt="" />
+                        </div>
                     </div>
-                    <div className="circle-area w-[100px] h-[100px] flex flex-col justify-center items-center">
-                        <div className="circle bg-gray-200 size-[20vw] rounded-full"></div>
+                    <div className="circle-area w-[100px] h-[100px] md:h-[250px] md:w-[250px] flex flex-col justify-center items-center">
+                        <div className="circle bg-gray-200 size-[65px] md:size-[150px] rounded-full overflow-hidden">
+                            <img src={p3} alt="" />
+                        </div>
                     </div>
-                    <div className="circle-area w-[100px] h-[100px] flex flex-col justify-center items-center">
-                        <div className="circle bg-gray-200 size-[20vw] rounded-full"></div>
+                    <div className="circle-area w-[100px] h-[100px] md:h-[250px] md:w-[250px] flex flex-col justify-center items-center">
+                        <div className="circle bg-gray-200 size-[65px] md:size-[150px] rounded-full overflow-hidden">
+                            <img src={p4} alt="" />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -29,33 +37,3 @@ function Hero() {
 }
 
 export default Hero
-
-{/* 
-                <div className="frames-slideshow h-[50vh] overflow-hidden w-full -mt-5 z-0">
-                    <div className="frames flex gap-[1px] overflow-hidden w-max">
-                        <div className="frame w-[50vw] h-[25vh] md:w-[250px] md:h-[50vh] rounded-tr-[50px] md:rounded-tr-[75px] hover:transform rounded-bl-[50px] md:rounded-bl-[75px] overflow-hidden">
-                            <img src={p1} alt="" />
-                        </div>
-                        <div className="frame w-[50vw] h-[25vh] md:w-[250px] md:h-[50vh] rounded-tl-[50px] md:rounded-tl-[75px] hover:transform rounded-br-[50px] md:rounded-br-[75px] overflow-hidden">
-                            <img src={p2} alt="" />
-                        </div>
-                        <div className="frame w-[50vw] h-[25vh] md:w-[250px] md:h-[50vh] rounded-tr-[50px] md:rounded-tr-[75px] hover:transform rounded-bl-[50px] md:rounded-bl-[75px] overflow-hidden">
-                            <img src={p3} alt="" />
-                        </div>
-                        <div className="frame w-[50vw] h-[25vh] md:w-[250px] md:h-[50vh] rounded-tl-[50px] md:rounded-tl-[75px] hover:transform rounded-br-[50px] md:rounded-br-[75px] overflow-hidden">
-                            <img src={p4} alt="" />
-                        </div>
-                        <div className="frame w-[50vw] h-[25vh] md:w-[250px] md:h-[50vh] rounded-tr-[50px] md:rounded-tr-[75px] hover:transform rounded-bl-[50px] md:rounded-bl-[75px] overflow-hidden">
-                            <img src={p1} alt="" />
-                        </div>
-                        <div className="frame w-[50vw] h-[25vh] md:w-[250px] md:h-[50vh] rounded-tl-[50px] md:rounded-tl-[75px] hover:transform rounded-br-[50px] md:rounded-br-[75px] overflow-hidden">
-                            <img src={p2} alt="" />
-                        </div>
-                        <div className="frame w-[50vw] h-[25vh] md:w-[250px] md:h-[50vh] rounded-tr-[50px] md:rounded-tr-[75px] hover:transform rounded-bl-[50px] md:rounded-bl-[75px] overflow-hidden">
-                            <img src={p3} alt="" />
-                        </div>
-                        <div className="frame w-[50vw] h-[25vh] md:w-[250px] md:h-[50vh] rounded-tl-[50px] md:rounded-tl-[75px] hover:transform rounded-br-[50px] md:rounded-br-[75px] overflow-hidden">
-                            <img src={p4} alt="" />
-                        </div>
-                    </div>
-                </div> */}
