849ed397b7d5396f8aeb7ab13200a067c3b7b3dd
d3v3sh 54ini <128877193+devesh-saini@users.noreply.github.com>
2024-08-11 11:58:02 +0530

    hero-section

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import './Hero.css'
 
 function Hero() {
     return (
         <>
-            <div className="heroMain">
-                
+            <div className="heroMain flex flex-col justify-between items-center h-1/2">
+                <div className="hero-subheading pt-24 text-red-800">
+                    <p>WELCOME TO</p>
+                </div>
+                <div className="hero-heading md:text-[20vw] leading-[175px]">
+                    <p>Uttam Attires</p>
+                </div>
             </div>
         </>
     )
