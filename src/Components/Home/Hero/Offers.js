6c5e4db7d10b81e40dae89ab9b9cf865022bc4ac
d3v3sh 54ini <128877193+devesh-saini@users.noreply.github.com>
2024-08-19 17:47:34 +0530

    offers

--- src/Components/Home/Hero/Offers.js
+++ src/Components/Home/Hero/Offers.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Offers() {
     return (
         <>
-            <div className="offers flex pt-5">
-                <div className="offer-left flex justify-center items-center w-1/2 p-5 border">
-                    <div className="content">
+            <div className="offers flex mt-5 bg-amber-500">
+                <div className="offer-left flex justify-center items-center w-1/2 p-5 border-y-2 border-amber-500 rounded-r-3xl bg-white">
+                    <div className="content overflow-hidden">
                         <div className="heading md:text-3xl text-xl">
                             Get 10% OFF
                         </div>
@@ -17,8 +17,8 @@ function Offers() {
                         </div>
                     </div>
                 </div>
-                <div className="offer-right flex justify-center items-center w-1/2 p-5 border">
-                    <div className="content">
+                <div className="offer-right flex justify-center text-white items-center w-1/2 p-5 border-amber-500">
+                    <div className="content overflow-hidden">
                         <div className="heading md:text-3xl text-xl">
                             Get FREE Delivery
                         </div>
