1f5db7afab127bbb18aa0c44e31c97b5ea6622ee
d3v3sh 54ini <128877193+devesh-saini@users.noreply.github.com>
2024-08-19 18:29:00 +0530

    featured

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,19 +1,50 @@
 import React from 'react'
 import Hero from './Hero'
 import Offers from './Offers'
+import p1 from '../Assets/HeroPics/TestPics/BG1.png'
+import p2 from '../Assets/HeroPics/TestPics/BG2.png'
+import p3 from '../Assets/HeroPics/TestPics/BG3.png'
+import p4 from '../Assets/HeroPics/TestPics/BG4.png'
+import ItemCard from '../../Essentials/ItemCard'
 
 function Home() {
+
+    const featured = [
+        {
+            img: p1,
+            price: 499
+        },
+        {
+            img: p2,
+            price: 499
+        },
+        {
+            img: p3,
+            price: 499
+        },
+        {
+            img: p4,
+            price: 499
+        }
+    ]
+
     return (
         <>
             <div className="homeMain">
                 <div className="heroMain max-h-[50vh]">
                     <Hero />
                 </div>
-                <div className="offersMain max-h-[50vh]">
+                <div className="offersMain max-h-[50vh] pb-5">
                     <Offers />
                 </div>
-                <div className="featured">
-
+                <div className="featured flex flex-wrap">
+                    {
+                        featured.map((item, index) => {
+                            return (
+                                <ItemCard img={item.img} price={item.price} />
+                            )
+                        })
+                    }
                 </div>
             </div>
         </>
--- src/Essentials/ItemCard.js
+++ src/Essentials/ItemCard.js
@@ -1,23 +1,19 @@
 import React from 'react'
+import './ItemCard.css'
 
-function ItemCard() {
+function ItemCard(props) {
   return (
     <>
-      <div className="mainBox">
+      <div className="mainBox p-2 w-40 md:w-1/4 border">
         <div className="allContent">
-          <div className="item-image">
-            
+          <div className="item-image overflow-hidden aspect-square">
+            <img src={props.img} alt="" />
           </div>
           <div className="title">
 
           </div>
-          <div className="properties">
-            <div className="price">
-
-            </div>
-            <div className="available-sizes">
-
-            </div>
+          <div className="price">
+            {props.price}
           </div>
         </div>
       </div>
