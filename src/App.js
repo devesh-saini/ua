5cd329ecb6c97b5e96d278f3b6862a87219305fa
d3v3sh 54ini <128877193+devesh-saini@users.noreply.github.com>
2024-08-25 19:23:48 +0530

    ItemCard

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Components/Home/Home'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
           </Routes>
+          <Footer />
         </Router>
       </div>
     </>
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+            <div className="main md:p-[50px] p-5 bg-amber-100">
+                <div className="heading text-center font-[Tangerine] text-6xl pt-[30px]">
+                    Uttam Attires
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
